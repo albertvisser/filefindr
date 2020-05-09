28cd5c62bfbd8d57396e74f248d2615f184d6fdb
Albert Visser <albert.visser@gmail.com>
2020-05-09 15:24:20 +0200

    #880: bepaling welke toolkit gebruikt wordt

--- afrift/gui.py
+++ afrift/gui.py
@@ -1,9 +1,8 @@
 """imports the gui code from a toolkit-specific module
 so that the other modules don't need to know which one is used
 """
-# from .afrift_ppgui import MainFrame
-# from .afrift_tkgui import MainFrame
-# from .afrift_tk3gui import MainFrame
-# from .afrift_qt4gui import MainFrame
-from .gui_wx import SelectNamesGui, ResultsGui, MainFrameGui
-# from .gui_qt import SelectNamesGui, ResultsGui, MainFrameGui
+from .toolkit import toolkit
+if toolkit == 'qt':
+    from .gui_qt import SelectNamesGui, ResultsGui, MainFrameGui
+elif toolkit == 'wx':
+    from .gui_wx import SelectNamesGui, ResultsGui, MainFrameGui
